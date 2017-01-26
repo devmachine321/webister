a27008280e51e88ed1350dd0a61f78f4eb02265d
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-01-26 02:46:23 +0000

    [New Design]

--- application/tmp/webister/interface/main.php
+++ application/tmp/webister/interface/main.php
@@ -21,46 +21,50 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.css">
-
-	
-	<div class="login-page bk-img">
-		<div class="form-content">
-			<div class="container">
-				<div class="row">
-				
-					<div class="col-md-6 col-md-offset-3">
-						<h1 class="text-center text-bold text-light mt-4x">Sign in to <img src="<?php echo file_get_contents("data/logo");?>"></h1>
-						<div class="well row pt-2x pb-3x bk-light">
-							<div class="col-md-8 col-md-offset-2">
-								<form action="index.php?page=val" class="mt" method="POST">
-									<?php if (isset($_GET["error"])) {?>
-<div class="alert alert-danger">
-  <strong>Danger!</strong> The username or password was not found.
-</div>
-<?php } ?>
-									<label for="" class="text-uppercase text-sm">Your Username or Email</label>
-									<input type="text" placeholder="Username" class="form-control mb" name="user">
-
-									<label for="" class="text-uppercase text-sm">Password</label>
-									<input type="password" placeholder="Password" class="form-control mb" name="pass">
-
-								
-									<button class="btn btn-primary btn-block" type="submit">LOGIN</button>
-	<a class="btn btn-primary btn-block" href="adminer-4.2.4.php">LOGIN TO MYSQL</a>
-
-								</form>
-							</div>
-						</div>
-						<div class="text-center text-light">
-							<a href="" class="text-light">Powered using <a href="http://alwaysontop617.github.io/webister/">Webister</a></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	
+   <Style>
+#page-wrap {
+     width: 800px;
+     margin: 0 auto;
+}
+     
+   </Style>
+   <div id="page-wrap">
+<h1>Sign in to <img src="<?php echo file_get_contents("data/logo");?>"></h1>
+  <div class="row">
+                  <div class="col-xs-6">
+                      <div class="well">
+                          <form id="loginForm" method="POST" action="index.php?page=val" novalidate="novalidate">
+                              <div class="form-group">
+                                  <label for="username" class="control-label">Username</label>
+                                  <input type="text" class="form-control" id="username" name="user" value="" required="" title="Please enter you username" placeholder="example@gmail.com">
+                                  <span class="help-block"></span>
+                              </div>
+                              <div class="form-group">
+                                  <label for="password" class="control-label">Password</label>
+                                  <input type="password" class="form-control" id="password" name="pass" value="" required="" title="Please enter your password">
+                                  <span class="help-block"></span>
+                              </div>
+                              <?php if (isset($_GET["error"])) {?>
+                              <div id="loginErrorMsg" class="alert alert-danger">Wrong username or password</div><?php } ?>
+                     
+                              <button type="submit" class="btn btn-success btn-block">Login</button>
+                              <a href="adminer-4.2.4.php" class="btn btn-default btn-block">SQL Manager</a>
+                          </form>
+                      </div>
+                  </div>
+                  <div class="col-xs-6">
+                      <p class="lead">Features of <span class="text-success">Webister</span></p>
+                      <ul class="list-unstyled" style="line-height: 2">
+                          <li><span class="fa fa-check text-success"></span> See all you SQL Databases</li>
+                          <li><span class="fa fa-check text-success"></span> SSL Security</li>
+                          <li><span class="fa fa-check text-success"></span> Anytime Anywhere</li>
+                          <li><span class="fa fa-check text-success"></span> Manage your entire website</li>
+                      </ul>
+                    
+                  </div>
+              </div>
+          </div>
+          </div>
 	<!-- Loading Scripts -->
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap-select.min.js"></script>
